2ae9cac871a98e2d29c89abe547a64c8ea805f53
Rohan Maharjan <rohan.maharjan@harness.io>
2024-01-27 11:39:38 +0545

    all good

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,5 +1,5 @@
 let BASE_URL = "http://localhost:5000";
-if (process.env.NODE_ENV == "development") {
+if (process.env.NODE_ENV !== "development") {
   BASE_URL = "https://portfolio-api-e7be.onrender.com";
 }
 
